a3664a6a0bc11e615a557249dfac602e5a9f84ff
Kevin <kzenstratus@uchicago.edu>
2018-12-05 23:42:50 -0600

    adding transform space and dot space funcs ... still need to work on getting the kernel

---
+++ app/utils/lin_alg.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def get2dDotSpace(xDomain, yDomain, numTicks):
+	xPoints = np.linspace(xDomain[0], xDomain[1], numTicks)
+	yPoints = np.linspace(yDomain[0], yDomain[1], numTicks)
+	allPoints = np.transpose([np.tile(xPoints, len(yPoints))
+		, np.repeat(yPoints, len(xPoints))])
+	return(allPoints)
+
+def getTransformSpace(space, transMatrix):
+	return (np.dot(space, transMatrix))
+
+
+def getKernelId(space, transMatrix):
+	'''
+	[Calculates the kernel, or all the points which get mapped to zero. 
+	This allows us to color the kernel.]
+	
+	@param {[1,2]d double array} space [a matrix representing the original space]
+	@param {[1,2]d double array} transMatrix [a transformation matrix]
+	@return {n x 2 double array} - a list of the kernel
+	'''
+	newSpace = getTransformSpace(space, transMatrix)
+
+	# Don't actually need to calculate the kernel. Just 
+	# take the space of points which go to 0.
+
+	newSpace
