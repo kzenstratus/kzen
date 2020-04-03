fe7a3dbb5c956fe9630367f067d8a199115bffdf
Kevin Zen <kevinzen@Kevins-MacBook-Pro.local>
2020-04-02 22:43:56 -0500

    got the curve correct

--- static/js/concepts/bessel/bessel_vars.js
+++ static/js/concepts/bessel/bessel_vars.js
@@ -93,14 +93,14 @@ var vecCoordJsonBesselBias = {
       , "style" : "solid"
   },
   "staticRightT": {
-    "coordList": Array(1).fill([[2, 0.5], [0,  0.5]]).concat(Array(5).fill([[5, 0], [5,  2]]))
+    "coordList": Array(1).fill([[2, 0.5], [0,  0.5]]).concat(Array(5).fill([[13, -4.5], [13,  -2.5]]))
   , "color": "#68a2ff"// orange // "#6bcc35"green
   // , "labels": Array(6).fill("a")
   // , "labelLoc": Array(6).fill([5, -10])
     , "style" : "solid"
 },
   "staticLeftT": {
-    "coordList": Array(1).fill([[-2, 0.5], [0,  0.5]]).concat(Array(5).fill([[5, 2], [5,  4]]))
+    "coordList": Array(1).fill([[-2, 0.5], [0,  0.5]]).concat(Array(5).fill([[13, -2.5], [13,  0.5]]))
   , "color": "#68a2ff"// orange // "#6bcc35"green
   // , "labels": Array(6).fill("a")
     , "style" : "solid"
@@ -112,14 +112,14 @@ let r = vecCoordJsonBesselBias.right.coordList
 let rT = [r[0]]
 for (let i = 1; i < r.length; i ++){
   let d = math.abs(r[i][0][0] - r[i][1][0]) // distance to mean
-  rT[i] = [[4, 0], [4, d]]
+  rT[i] = [[12, 0 - 4.5], [12, d - 4.5]]
 }
 // Add left vertical vector
 let l = vecCoordJsonBesselBias.left.coordList
 let lT = [l[0]]
 for (let i = 1; i < l.length; i ++){
   let d = math.abs(l[i][0][0] - l[i][1][0]) // distance to mean
-  lT[i] = [rT[i][1], [4, rT[i][1][1] + d]]
+  lT[i] = [rT[i][1], [12, rT[i][1][1] + d]]
 }
 vecCoordJsonBesselBias["rightT"] = {"coordList" : rT
                                   , "color" :  "#FD5F00"// orange
@@ -220,7 +220,7 @@ besselBiasVarPayload = {
         // ,[2, 48 + besselBiasVarShift]
         [-3 , 18 + besselBiasVarShift]
         ,[-2 , 8 + besselBiasVarShift]
-        ,[-1, 2 + besselBiasVarShift]
+        ,[-1 , 2 + besselBiasVarShift]
         ,[0, 0 + besselBiasVarShift]
         ,[1, 2 + besselBiasVarShift]
         ,[2, 8 + besselBiasVarShift]
